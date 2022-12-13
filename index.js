fdbb874c2069988a56d99d27a1a330670114774d
HDS <hds.sspca3@gmail.com>
2022-12-13 18:51:32 +0700

    add function to add data to JSON file

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import * as url from 'url';
 import express from "express";
+import fs from "fs";
+import path from 'path';
 
 const PORT = process.env.PORT || 5115;
 
@@ -13,9 +15,41 @@ const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 const server = express();
 server.use(express.static(__dirname + "/views"));
 
+
+function addDataToJSON() {
+   // read json data asynchronously
+   fs.readFile(path.resolve(__dirname, 'data.json'), (err, data) => {
+      if (err) {
+        // handle the error
+        console.log(err)
+      } else {
+        // parse the JSON data
+        let jsonData = JSON.parse(data);
+        console.log(jsonData)
+        // add the new data to the JSON object
+        jsonData.newData = 'my new data';
+         // write the JSON data back to the file
+        fs.writeFile(path.resolve(__dirname, 'data.json'), JSON.stringify(jsonData), (err) => {
+         if (err) {
+           // handle the error
+           console.log(err)
+         } else {
+           // data has been written successfully
+         }
+       });
+      }
+    });
+}
+
+
 server.get("/", (req, res) => {
    // sendfile from views folder
    res.sendFile(__dirname + "/views/index.html");
+})
+
+server.get("/test", (req, res) => {
+   addDataToJSON()
+   res.end()
 })
 
 
