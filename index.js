68f061c820c427912df22586e5480520dc8a346f
HDS <hds.sspca3@gmail.com>
2022-12-13 20:46:10 +0700

    add function to delete data in data.json

--- index.js
+++ index.js
@@ -43,6 +43,40 @@ function addDataToJSON(ticketOrder) {
   });
 }
 
+function deleteDataFromJSON(ticketOrderIdToDelete) {
+  // Read the JSON file
+  fs.readFile(path.resolve(__dirname, "data.json"), (err, data) => {
+    if (err) {
+      // Handle the error
+      console.log(err);
+      return;
+    }
+
+    // Parse the JSON data
+    const jsonData = JSON.parse(data);
+
+    // Use the filter method to create a new array of orders
+    // that does not include the order with the specified orderId
+    const updatedOrders = jsonData.orders.filter(
+      (order) => order.orderId !== orderIdToDelete
+    );
+
+    // Overwrite the existing orders with the updated orders
+    jsonData.orders = updatedOrders;
+
+    // Write the updated data back to the file
+    fs.writeFile("data.json", JSON.stringify(jsonData), (err) => {
+      if (err) {
+        // Handle the error
+        console.log(err);
+        return;
+      }
+
+      // The data has been successfully updated
+    });
+  });
+}
+
 server.get("/", (req, res) => {
   // sendfile from views folder
   res.sendFile(__dirname + "/views/index.html");
@@ -76,6 +110,11 @@ const ticketOrder = {
 
 server.get("/addTicket", (req, res) => {
   addDataToJSON(ticketOrder);
+  res.end();
+});
+
+server.get("/deleteTicket", (req, res) => {
+  deleteDataFromJSON("321");
   res.end();
 });
 
