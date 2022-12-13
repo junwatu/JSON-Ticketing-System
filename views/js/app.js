4cf17bfab1356fcff8b7dabdef9aa123dbf42339
HDS <hds.sspca3@gmail.com>
2022-12-14 05:21:56 +0700

    get json data for main app title

--- views/js/app.js
+++ views/js/app.js
@@ -1,9 +1,22 @@
 "use strict";
 
+const useState = React.useState;
+const useEffect = React.useEffect;
+
 function App() {
+  const [concert, setConcert] = useState(null);
+  useEffect(() => {
+    fetch("/concert")
+      .then((response) => response.json())
+      .then((data) => {
+        setConcert(data);
+        console.log(concert);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <h1>Concert of the Century</h1>
+      <h1>{concert ? concert.event.name : "loading"}</h1>
     </div>
   );
 }
