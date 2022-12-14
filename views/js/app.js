b619633936f83a295694e60245e88f8647364733
HDS <hds.sspca3@gmail.com>
2022-12-14 08:00:34 +0700

    add order ticket form and save it to the server

--- views/js/app.js
+++ views/js/app.js
@@ -3,21 +3,112 @@
 const useState = React.useState;
 const useEffect = React.useEffect;
 
+function generateOrderId() {
+  // Generate a random number between 100000 and 999999
+  let orderId = Math.floor(Math.random() * 900000) + 100000;
+
+  // Convert the number to a string
+  orderId = orderId.toString();
+
+  // Keep only the first 6 digits
+  orderId = orderId.substring(0, 6);
+
+  return orderId;
+}
+
 function App() {
   const [concert, setConcert] = useState(null);
+  const [ticketType, setTicketType] = useState(null);
+  const [quantity, setQuantity] = useState(0);
+
   useEffect(() => {
     fetch("/concert")
       .then((response) => response.json())
       .then((data) => {
         setConcert(data);
-        console.log(concert);
       });
   }, []);
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const tickets = [];
+    let id = generateOrderId();
+
+    tickets.push({
+      type: ticketType.order.split(":")[0],
+      price: ticketType.order.split(":")[1],
+      quantity: quantity,
+    });
+
+    const ticketOrder = {
+      orderId: id,
+      tickets,
+      totalCost: quantity * ticketType.order.split(":")[1],
+    };
+
+    // Send the data to the server using the fetch API
+    fetch("/addTicket", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(ticketOrder),
+    }).catch((err) => {
+      console.log(err);
+    });
+  };
+
   return (
-    <div className="App">
-      <h1>{concert ? concert.event.name : "loading"}</h1>
-    </div>
+    <>
+      <div className="text-start">
+        <h1>{concert ? concert.event.name : "loading"}</h1>
+        <p className="h4">{concert ? concert.event.location : "loading"}</p>
+        <p className="h6">{concert ? concert.event.date : "loading"}</p>
+      </div>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label className="form-label">Ticket</label>
+          <TicketSelect
+            ticketTypes={concert ? concert.ticketTypes : []}
+            onChange={(event) => {
+              setTicketType({
+                order: event.target.value,
+              });
+            }}
+          />
+        </div>
+        <div className="mb-3">
+          <label className="form-label">Quantity</label>
+          <input
+            type="number"
+            className="form-control"
+            onChange={(event) => setQuantity(event.target.value)}
+          />
+        </div>
+
+        <button type="submit" className="btn btn-primary">
+          Save
+        </button>
+      </form>
+    </>
+  );
+}
+
+function TicketSelect(props) {
+  const ticketTypes = props.ticketTypes;
+  const onChangeHandler = props.onChange;
+
+  return (
+    <select className="form-select" onChange={onChangeHandler}>
+      {ticketTypes.map((ticketType) => (
+        <option
+          key={ticketType.id}
+          value={`${ticketType.type}:${ticketType.price}`}
+        >
+          {ticketType.type} - ${ticketType.price}
+        </option>
+      ))}
+    </select>
   );
 }
 
